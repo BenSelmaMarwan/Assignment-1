a46958a7017c0b898a63b0b6cd3c9ae60d904069
Marwan <marwan.benselma@gmail.com>
2023-09-22 05:31:02 +0200

    creation of html file

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello from index.js!");
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3500;
+
+// Middleware to disable caching
+app.use((req, res, next) => {
+    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.header('Pragma', 'no-cache');
+    res.header('Expires', '0');
+    next();
+});
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define the route to serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
